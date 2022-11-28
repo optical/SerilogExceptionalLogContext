a26b89dbdb890d4f566103daa7f9dfa0e46a9355
Michael Grundberg <michael@mokon.se>
2022-11-28 17:02:50 +0100

    Small optimization

--- Serilog.ExceptionalLogContext/ExceptionContextEnricher.cs
+++ Serilog.ExceptionalLogContext/ExceptionContextEnricher.cs
@@ -16,9 +16,9 @@ namespace Serilog.ExceptionalLogContext {
 
 		private void CurrentDomain_FirstChanceException(object sender, FirstChanceExceptionEventArgs exceptionEvent) {
 			try {
-				var context = LogContext.Clone();
 				if (!_exceptionToContextLookup.TryGetValue(exceptionEvent.Exception, out var _))
 				{
+					var context = LogContext.Clone();
 					// Call Add, not AddOrUpdate. If an exception is logged twice, the context from the original callsite will be preserved.
 					// This is desireable as subsequent throws/logs of the exception will unwind the stack, removing the context we're trying to preserve
 					// Note: this can throw, which absoutely necessitates the try/catch
